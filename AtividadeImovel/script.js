6a9991094d0cf10361a263c4cd2a760daa4ef86c
gugs(Gustavo.L) <123033640+gugsinwork@users.noreply.github.com>
2023-10-06 15:58:01 -0300

    Update script.js

--- AtividadeImovel/script.js
+++ AtividadeImovel/script.js
@@ -1,26 +1,32 @@
 function calcularImovel(metragem, quartos){
     let m2 = 3000;
     let preco = 0;
-
+    quartos = window.prompt('informe a quantidade de quartos desse imóvel: ')
     switch (metragem, quartos) {
         case 1:
-        default:
-           preco = metragem * (m2*1)
-            break;
+            default:
+                metragem = parseFloat(window.prompt('informe quantos m2 o imóvel tem: '))
+                preco = metragem * (m2*1)
+                console.log(`o preço é R$${preco}`)
+                break;
         case 2:
+            metragem = parseFloat(window.prompt('informe quantos m2 o imóvel tem: '))
             preco = metragem * (m2*1.2)
+            console.log(`o preço é R$${preco}`)
            
         case 3:
+            metragem = parseFloat(window.prompt('informe quantos m2 o imóvel tem: '))
             preco = metragem * (m2*1.5)
+            console.log(`o preço é R$${preco}`)
             break;
         case quartos > 3:
+            metragem = parseFloat(window.prompt('informe quantos m2 o imóvel tem: '))
             preco = metragem * (m2*2)
+            console.log(`o preço é R$${preco}`)
             break;
     }
     return preco
 }
 
-metragem = window.prompt('informe quantos m2 o imóvel tem: ')
-quartos = window.prompt('informe a quantidade de quartos desse imóvel: ')
-let calculaImovel = calcularImovel(metragem, quartos)
-console.log(`o preço é R$${calculaImovel}`)
+console.log(calcularImovel())
+
